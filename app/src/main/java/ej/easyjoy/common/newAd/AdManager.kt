0a9ca233e096781ce715e3e8ccb5911e762fa718
xpon <809998053@qq.com>
2021-12-24 16:53:22 +0800

    广告及给我好评等部分统一功能独立成库

--- app/src/main/java/ej/easyjoy/common/newAd/AdManager.kt
+++ app/src/main/java/ej/easyjoy/common/newAd/AdManager.kt
@@ -2,6 +2,7 @@ package ej.easyjoy.common.newAd
 
 import android.app.Activity
 import android.content.Context
+import android.content.SharedPreferences
 import android.view.ViewGroup
 import com.bytedance.msdk.api.v2.*
 import com.bytedance.sdk.openadsdk.TTAdConfig
@@ -37,8 +38,15 @@ class AdManager {
 
     private val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd")
 
-    fun showAdForAuditing(): Boolean {
-        return simpleDateFormat.format(Date()) >= "2021-11-20"
+    fun showAdForAuditing(context: Context): Boolean {
+        val sp = context.getSharedPreferences("data", Context.MODE_MULTI_PROCESS)
+        if(sp.getLong("first_ad_time",0L)==0L){
+            sp.edit().putLong("first_ad_time",System.currentTimeMillis())
+        }
+        if(System.currentTimeMillis()-sp.getLong("first_ad_time",0L)>(24*60*60*1000)){
+            return true
+        }
+        return false
     }
 
     fun initGMAdSdk(context: Context, appId: String){
@@ -63,19 +71,20 @@ class AdManager {
                 .build())
     }
 
-    fun initTTAdSdk(context: Context,appId: String){
+    fun initTTAdSdk(context: Context, appId: String){
         TTAdSdk.init(context, TTAdConfig.Builder()
-                        .appId(appId)
-                        .useTextureView(false) //使用TextureView控件播放视频,默认为SurfaceView,当有SurfaceView冲突的场景，可以使用TextureView
-                        .appName(context.resources.getString(R.string.app_name))
-                        .titleBarTheme(TTAdConstant.TITLE_BAR_THEME_DARK)
-                        .allowShowNotify(true) //是否允许sdk展示通知栏提示
-                        .allowShowPageWhenScreenLock(true) //是否在锁屏场景支持展示广告落地页
-                        .directDownloadNetworkType() //允许直接下载的网络状态集合
-                        .supportMultiProcess(false) //是否支持多进程，true支持
-                        .build(), object : TTAdSdk.InitCallback {
+                .appId(appId)
+                .useTextureView(false) //使用TextureView控件播放视频,默认为SurfaceView,当有SurfaceView冲突的场景，可以使用TextureView
+                .appName(context.resources.getString(R.string.app_name))
+                .titleBarTheme(TTAdConstant.TITLE_BAR_THEME_DARK)
+                .allowShowNotify(true) //是否允许sdk展示通知栏提示
+                .allowShowPageWhenScreenLock(true) //是否在锁屏场景支持展示广告落地页
+                .directDownloadNetworkType() //允许直接下载的网络状态集合
+                .supportMultiProcess(false) //是否支持多进程，true支持
+                .build(), object : TTAdSdk.InitCallback {
             override fun success() {
             }
+
             override fun fail(p0: Int, p1: String?) {
             }
         })
@@ -85,28 +94,28 @@ class AdManager {
         GDTAdSdk.init(context, appId)
     }
 
-    fun initKSAdSdk(context: Context,appId: String){
+    fun initKSAdSdk(context: Context, appId: String){
 
     }
 
     fun showSplashAd(activity: Activity?, adLayout: ViewGroup?, qqId: String?, ttId: String?, gromoreId: String, adListener: AdListener?) {
-        if (showAdForAuditing()) {
+        if (showAdForAuditing(activity!!)) {
             val splashAd = SplashAd(activity!!, adLayout!!, qqId!!, ttId!!, gromoreId!!, adListener!!)
             splashAd.showSplashAd()
         }
     }
 
-    fun showGMSplashAd(activity: Activity, adContainer: ViewGroup,groMoreId: String,defaultAppId: String, defaultId: String,adListener: AdListener){
+    fun showGMSplashAd(activity: Activity, adContainer: ViewGroup, groMoreId: String, defaultAppId: String, defaultId: String, adListener: AdListener){
         var gmSplashAd = GMSplashAd()
-        if(!showAdForAuditing()){
+        if(!showAdForAuditing(activity)){
             adListener.adError("")
             return
         }
-        gmSplashAd.showSplashAd(activity,adContainer,groMoreId,defaultAppId,defaultId,adListener)
+        gmSplashAd.showSplashAd(activity, adContainer, groMoreId, defaultAppId, defaultId, adListener)
     }
 
     fun showGMBannerAd(activity: Activity, adContainer: ViewGroup, groMoreId: String, adListener: AdListener): GMBannerAd?{
-        if (showAdForAuditing()) {
+        if (showAdForAuditing(activity)) {
             val gmBannerAd = GMBannerAd()
             gmBannerAd.showBannerAd(activity, adContainer, groMoreId, adListener)
             return gmBannerAd
@@ -115,7 +124,7 @@ class AdManager {
     }
 
     fun showGMNativeAd(context: Context, adContainer: ViewGroup, groMoreId: String, adListener: AdListener): GMNativeAd?{
-        if (showAdForAuditing()) {
+        if (showAdForAuditing(context)) {
             val gmNativeAd = GMNativeAd()
             gmNativeAd.showNativeAd(context, adContainer, groMoreId, adListener)
             return gmNativeAd
@@ -124,7 +133,7 @@ class AdManager {
     }
 
     fun showGMInterstitialAd(activity: Activity, groMoreId: String, adListener: AdListener): GMInterstitialAd?{
-        if (showAdForAuditing()) {
+        if (showAdForAuditing(activity)) {
             val gmInterstitialAd = GMInterstitialAd()
             gmInterstitialAd.showInteractionAd(activity, groMoreId, adListener)
             return gmInterstitialAd
@@ -132,17 +141,17 @@ class AdManager {
         return null
     }
 
-    fun showGMFullVideoAd(activity: Activity, groMoreId: String,adListener: AdListener): GMFullVideoAd? {
-        if (showAdForAuditing()) {
+    fun showGMFullVideoAd(activity: Activity, groMoreId: String, adListener: AdListener): GMFullVideoAd? {
+        if (showAdForAuditing(activity)) {
             val fullVideoAd = GMFullVideoAd()
-            fullVideoAd.showFullVideoAd(activity,groMoreId,adListener)
+            fullVideoAd.showFullVideoAd(activity, groMoreId, adListener)
             return fullVideoAd
         }
         return null
     }
 
     fun showInterstitialAd(activity: Activity, qqId: String, ttId: String, groMoreId: String, adListener: AdListener): InterstitialAd? {
-        if (showAdForAuditing()) {
+        if (showAdForAuditing(activity)) {
             val interstitialAd = InterstitialAd(activity, qqId, ttId, groMoreId, adListener)
             interstitialAd.showInterstitialAD()
             return interstitialAd
@@ -150,16 +159,16 @@ class AdManager {
         return null
     }
     fun showNativeAd(activity: Context?, adContainer: ViewGroup?, qqId: String?, ttId: String?, ksId: Long, groMoreId: String, adListener: AdListener?): NativeAD? {
-        if (showAdForAuditing()) {
+        if (showAdForAuditing(activity!!)) {
             val nativeAd = NativeAD()
-            nativeAd.showNativeAd(activity!!,adContainer!!,groMoreId,adListener!!)
+            nativeAd.showNativeAd(activity!!, adContainer!!, groMoreId, adListener!!)
             return nativeAd
         }
         return null
     }
 
     fun showBannerAd(activity: Activity?, adContainer: ViewGroup?, qqId: String?, ttId: String?, groMoreId: String, adListener: AdListener?): BannerAd? {
-        if (showAdForAuditing()) {
+        if (showAdForAuditing(activity!!)) {
             val bannerAd = BannerAd()
             bannerAd.showBannerAd(activity, adContainer, qqId, ttId, groMoreId, adListener)
             return bannerAd
@@ -169,7 +178,7 @@ class AdManager {
 
 
     fun showTTNativeAd(activity: Activity?, adContainer: ViewGroup?, ttId: String?, adListener: AdListener?): NativeAD? {
-        if (showAdForAuditing()) {
+        if (showAdForAuditing(activity!!)) {
             val nativeAd = NativeAD()
             nativeAd.showTTNativeAd(activity, adContainer, ttId, adListener)
             return nativeAd
@@ -178,7 +187,7 @@ class AdManager {
     }
 
     fun showQQNativeAd(activity: Activity?, adContainer: ViewGroup?, ttId: String?, adListener: AdListener?): NativeAD? {
-        if (showAdForAuditing()) {
+        if (showAdForAuditing(activity!!)) {
             val nativeAd = NativeAD()
             nativeAd.showQQNativeAd(activity, adContainer, ttId, adListener)
             return nativeAd
@@ -188,7 +197,7 @@ class AdManager {
 
 
     fun showGroMoreNativeAd(activity: Activity, adContainer: ViewGroup, groMoreId: String, adListener: AdListener): NativeAD? {
-        if (showAdForAuditing()) {
+        if (showAdForAuditing(activity)) {
             val nativeAd = NativeAD()
             nativeAd.showGroMoreNativeAd(activity, adContainer, groMoreId, adListener)
             return nativeAd
@@ -198,9 +207,9 @@ class AdManager {
 
 
 
-    fun showFullVideoAd(activity: Activity, qqId: String, ttId: String, groMoreId: String,adListener: AdListener): FullVideoAd? {
-        if (showAdForAuditing()) {
-            val fullVideoAd = FullVideoAd(activity, qqId, ttId, groMoreId,adListener)
+    fun showFullVideoAd(activity: Activity, qqId: String, ttId: String, groMoreId: String, adListener: AdListener): FullVideoAd? {
+        if (showAdForAuditing(activity)) {
+            val fullVideoAd = FullVideoAd(activity, qqId, ttId, groMoreId, adListener)
             fullVideoAd.showFullVideoAd()
             return fullVideoAd
         }

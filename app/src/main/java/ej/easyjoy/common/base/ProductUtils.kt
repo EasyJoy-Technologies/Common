c20bb4218ea859d5e21cb57f321570fabcb0d135
xpon <809998053@qq.com>
2022-01-06 14:23:55 +0800

    广告及给我好评等部分统一功能独立成库

--- app/src/main/java/ej/easyjoy/common/base/ProductUtils.kt
+++ app/src/main/java/ej/easyjoy/common/base/ProductUtils.kt
@@ -5,7 +5,7 @@ import ej.easyjoy.common.R
 
 object ProductUtils {
 
-    fun getOurProduct(): List<Product>{
+    fun getOurProduct(packName: String): List<Product>{
         var products = ArrayList<Product>()
         products.add(Product(R.mipmap.product_easy_note_pro_icon,"易趣记事本","超简洁好用的记事本","ej.easyjoy.easynote.cn"))
         products.add(Product(R.mipmap.product_cal_icon,"全能计算器","史上功能最全体验最好的计算器","ej.easyjoy.multicalculator.cn"))
@@ -14,7 +14,13 @@ object ProductUtils {
         products.add(Product(R.mipmap.product_easy_mirror_icon,"易趣镜子","让手机变成您的随身化妆镜","ej.easyjoy.easymirror"))
         products.add(Product(R.mipmap.product_easy_locker_icon,"悬浮锁屏","悬浮球快捷功能虚拟按键","ej.easyjoy.easylocker.cn"))
         products.add(Product(R.mipmap.product_easy_noise_icon,"噪音检测仪","为您检测环境噪音值","ej.easyjoy.noisechecker.cn"))
+        products.add(Product(R.mipmap.product_easy_tools_icon,"工具大全","单位换算各种查询常用工具大全","ej.easyjoy.toolsbox.cn"))
         products.add(Product(R.mipmap.product_easy_text_icon,"易记事","纯文本记事工具","ej.easyjoy.easynote.text.cn"))
+        for(product in products){
+            if(product.packageName == packName){
+                products.remove(product)
+            }
+        }
         return products
     }
 }
